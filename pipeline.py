22c5775929e6e59ee6856aa4081114a32c54722e
samthakur587 <samunder268@gmail.com>
2024-04-17 19:31:49 +0530

    first commit

---
+++ pipeline.py
@@ -0,0 +1,14 @@
+import subprocess
+
+print("start setup the utils")
+
+# Run the Python file
+subprocess.run(['python', 'utils.py'])
+
+print("start processing the video")
+
+subprocess.run(['python', 'video_procees.py'])
+
+print('start extrecting the data from frame by Easy OCR')
+
+subprocess.run(['python', 'ocr.py'])
